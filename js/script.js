b3bcccd0705e9efd6c18f053d51ed569c90b5aed
Gustavo Dias <gustavoalvesdiaz08@gmail.com>
2023-09-02 17:38:41 -0300

    Primeira seção implementada

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
 const toggleButton = document.querySelector(".mobile-toggle");
-const menu = document.querySelector("#primary-section nav");
+const menu = document.querySelector("#primary-section");
+
+function checkActive() {
+  if (menu.classList.contains("nav-active")) {
+    toggleButton.querySelector("img").src = "../images/icon-close.svg";
+  } else {
+    toggleButton.querySelector("img").src = "../images/icon-hamburger.svg";
+  }
+}
 
 toggleButton.addEventListener("click", () => {
-  menu.classList.toggle("active");
+  menu.classList.toggle("nav-active");
+  checkActive();
 });
+
+checkActive();
